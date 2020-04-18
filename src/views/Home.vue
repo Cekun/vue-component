c1d1bc5b5ad45e08edea4fb310bae4fdfdc08bb3
cekun <cekung@gmail.com>
2020-04-18 18:13:00 +0800

    项目开始了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    开始了
   </div>
 </template>
 
