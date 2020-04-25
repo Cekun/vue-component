e13cfe4e7dc240f296360dc85c50221d2e7a5992
cekun <cekung@gmail.com>
2020-04-25 23:23:07 +0800

    弹框组价

---
+++ src/components/Notice/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div v-if="isShow">
+        <h3>{{title}}</h3>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: ""
+            },
+            message: {
+                type: String
+            },
+            duration: {
+                type: Number
+            }
+        },
+        data() {
+            return {
+                isShow: false
+            }
+        },
+        methods: {
+            show() {
+                this.isShow = true;
+                setTimeout(() => {
+                    this.hide();
+                }, this.duration);
+            },
+            hide() {
+                this.isShow = false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
