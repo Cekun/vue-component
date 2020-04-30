91898d9340a1204ffc98a9d9431f651ca8fec613
cekun <cekung@gmail.com>
2020-04-30 15:57:58 +0800

    vuex test

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
   mutations: {
+    updateCount(state, num) {
+      state.count += num;
+    }
+  },
+  getters: {
+    score(state) {
+      return 'score: ' + state.count;
+    }
   },
   actions: {
+    //复杂业务逻辑，类似于controller
+    //比如ajax请求
+    // 模拟ajax
+    asyncAdd({commit, state}, num) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          commit('updateCount', num);
+          resolve({ok:1});
+        }, 1000);
+      })  
+    }
   },
   modules: {
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>vuex-test-count: {{$store.state.count}}</p>
+    <button @click="addCount">Add</button>
+    {{$store.getters.score}}
+    <button @click="asyncAdd">AsyncAdd</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    addCount() {
+      this.$store.commit('updateCount', 2)  //commit是mutation
+    },
+    async asyncAdd() {
+      const result = await this.$store.dispatch('asyncAdd', 3)  // dispatch是action
+      if(result.ok === 1){
+        alert('act success!')
+      }
+    }
+  },
+}
+</script>
